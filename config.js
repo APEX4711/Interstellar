b319bcd4bc1c9166085981a4e52863696ec211b8
APEX4711 <96887872+APEX4711@users.noreply.github.com>
2024-03-26 12:17:29 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,19 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
     interstellar: 'password',
+    ethanisthegoat: 'ethanisthegoat',
+    headgame: '10/10',
+    ilikelittleboys: 'ilikelittleboys',
+    fuckteachers: 'ihateteachers',
+    fuckallteachers: 'fuckallteachers',
+    drake: '10inchdick',
+    brownmonster: 'brownmonster',
+    suckmydickbitch: 'suckmydickbitch',
+    jaydenisabitch: 'jaydenisabitch',
+    thug: 'thug',
+    bandforband: 'bandforband',
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
